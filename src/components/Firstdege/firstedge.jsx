802219bf6fcb6e1ea361c4a9453a0477fd800be8
posong12 <a0939222122@gmail.com>
2024-05-09 19:33:19 +0800

    首頁更新

---
+++ src/components/Firstdege/firstedge.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/home.css';
+
+function App() {
+  return (
+    <div className="navbar">
+      <button className="home_btn">最新消息</button>
+      <button className="home_btn">簡答題評分系統</button>
+      <button className="home_btn">關於我們</button>
+      <button className="home_btn">聯絡我們</button>
+      <button className="home_btn">登入</button>
+    </div>
+  );
+}
+
+export default App;
